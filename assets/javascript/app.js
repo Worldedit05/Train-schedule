732405d2fcc1085ccdbfb5fc6bc65c82e16c2c05
Aaron Lee <worldedit05@gmail.com>
2016-09-12 00:32:59 -0500

    Setup files

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBT4DLqy8oVxmxgEBRf_NrKt30FwV4Y_nY",
+  authDomain: "train-schedules.firebaseapp.com",
+  databaseURL: "https://train-schedules.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
+
+
+// IMPORTANT!
+// Don't for get to set security rules in Firebase to true for testing!!
+//
+//
